5f29dc1e84cb80f7f78f7c8d55d94834f44841ca
Jordan <138933858+Roebot926@users.noreply.github.com>
2023-09-22 15:30:16 -0400

    Baby steps, have to beat record of 2 commits from last assignment. Shooting for 3.

--- queries.sql
+++ queries.sql
@@ -1,7 +1,17 @@
 -- Part 1: Test it with SQL
 
 -- Part 2: Test it with SQL
+SELECT * FROM job;
 
+SELECT name FROM employer WHERE location = "St. Louis City";
+
+DROP TABLE job;
+
+SELECT *
+FROM skill
+        (LEFT|INNER) JOIN job_skills ON (skill.id = job_skills.skills_id | job_skills.skills_id = skill.id)
+        (WHERE job_skills.jobs_id IS NOT NULL)
+        * ORDER BY name ASC;
 -- Part 3: Test it with SQL
 
 -- Part 4: Test it with SQL
